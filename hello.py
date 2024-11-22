dc723c01c22c9f83b903508e67e85cf877199e92
Zine Falouti <falouti.falouti@gmail.com>
2024-11-22 18:18:31 +0100

    Add str total

--- hello.py
+++ hello.py
@@ -3,9 +3,20 @@ def main():
     second = ask("What's your age? ")
     third = ask("What's your objective? ")
     print(f"Your name is {first}, your age is {second}, and your objective is {third}")
+    words = []
+    words += first
+    words += second
+    words += third
+    print("_"*30)
+    listing(words)
+    
 
 def ask(message):
     return input(message)
+
+def listing(list):
+    total = len(list)
+    print(f"Total str is: {total}")
 
 if __name__ == "__main__":
     main()
